fddd103c407a82594136765f3f0a6f51495a61f3
therealeddy <eduardo.mfonseca@hotmail.com>
2021-05-04 21:48:58 -0300

    feat: comunicacao complete

--- wp-content/themes/wordeddy/js/functions.js
+++ wp-content/themes/wordeddy/js/functions.js
@@ -4,11 +4,12 @@ const scrollToTop = () => {
 		return false;
 	});
 }
-const animation = () => {
+
+const galleryAnimationScroll = () => {
 	
 	gsap.registerPlugin(ScrollTrigger);
 
-	const sections = gsap.utils.toArray(".column-gallery");
+	const sections = gsap.utils.toArray(".item-galerry");
 	let maxWidth = 0;
 
 	const getMaxWidth = () => {
@@ -44,7 +45,74 @@ const animation = () => {
 	});
 }
 
+const hideShowAnimationScroll = () => {
+	function animateFrom(elem, direction) {
+		direction = direction || 1;
+		var x = 0,
+				y = direction * 100;
+		if(elem.classList.contains("gs_reveal_fromLeft")) {
+			x = -200;
+			y = 0;
+		} else if (elem.classList.contains("gs_reveal_fromRight")) {
+			x = 200;
+			y = 0;
+		} else if (elem.classList.contains("gs_reveal_fromTop")) {
+			x = 0;
+			y = -200;
+		}
+		elem.style.transform = "translate(" + x + "px, " + y + "px)";
+		elem.style.opacity = "0";
+		gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+			duration: 7, 
+			x: 0,
+			y: 0, 
+			autoAlpha: 1, 
+			ease: "expo", 
+			overwrite: "auto"
+		});
+	}
+	
+	function hide(elem) {
+		gsap.set(elem, {autoAlpha: 0});
+	}
+	
+	gsap.registerPlugin(ScrollTrigger);
+	
+	gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
+		hide(elem);
+		
+		ScrollTrigger.create({
+			trigger: elem,
+			onEnter: function() { animateFrom(elem) }, 
+			onEnterBack: function() { animateFrom(elem, -1) },
+			onLeave: function() { hide(elem) }
+		});
+	});
+}
+
+const parallaxAnimationScroll = () => {
+	gsap.to(".contentParallax", {
+		yPercent: -100,
+		ease: "none",
+		scrollTrigger: {
+			trigger: ".sectionParallax",
+			start: "top bottom",
+			end: "bottom top",
+			scrub: true
+		}, 
+	});
+}
+
+const actionsModal = () => {
+	jQuery('.modal-video').on('hidden.bs.modal', function (e) {
+		jQuery('.modal-video video').trigger('pause');;
+	})
+}
+
 jQuery('document').ready(function(){
 	scrollToTop();
-	animation();
+	galleryAnimationScroll();
+	hideShowAnimationScroll();
+	parallaxAnimationScroll();
+	actionsModal();
 });
--- wp-content/themes/wordeddy/templates/comunicacao.php
+++ wp-content/themes/wordeddy/templates/comunicacao.php
@@ -4,42 +4,13 @@
  */
 get_header(); ?>
 
-<style>
-
-.delayed-section {
-  position: absolute;
-  width: 30vw;
-  height: 38.87vw;
-}
-.delayed-section .inner-container {
-  will-change: transform;
-}
-.delayed-section img {
-  max-width: 100%;
-  will-change: transform;
-}
-
-#del1 {
-  top: 101vh;
-  left: 10vw;
-}
-#del2 {
-  top: 110vh;
-  left: 60vw;
-}
-#del3 {
-  top: 120vh;
-  left: 30vw;
-}
-</style>
-
 <div class="comunicacao">
   <div class="banner-comunicacao d-flex align-items-center">
     <video autoplay muted loop>
       <source src="<?php the_field('video_banner'); ?>" type="video/mp4">
     </video>
     <div class="container d-flex justify-content-center">
-      <h1> <?php the_field('titulo_banner'); ?> </h1>
+      <h1 class="animate__animated animate__fadeInDown"> <?php the_field('titulo_banner'); ?> </h1>
     </div>
   </div>
 
@@ -59,28 +30,63 @@ get_header(); ?>
   <?php endif; ?>
 
   <?php if( have_rows('destaques') ): ?>
-    <div class="produtos">
-      <div class="container-person">
-        <div class="container">
-          <div class="back-blue"></div>
-        </div>
+    <div class="back-blue">
+      <div class="container">
+        <div class="content"></div>
       </div>
-      <div class="gallery" style="background-image: url(<?php bloginfo('template_directory'); ?>/images/back-images.png)">
-        <?php while( have_rows('destaques') ): the_row(); ?>
-          <?php if( have_rows('coluna') ): ?>
-            <div class="column-gallery">
-              <?php while( have_rows('coluna') ): the_row(); ?>
-                <div class="item-galerry">
-                  <img src="<?php the_sub_field('imagem'); ?>" alt="photo">
-                </div>
-              <?php endwhile; ?>
-            </div>
+    </div>
+    <div class="gallery">
+      <?php while( have_rows('destaques') ): the_row(); ?>
+        <div class="item-galerry">
+          <?php if(get_sub_field('arquivo')['type'] === 'video'): ?>
+            <video autoplay muted loop>
+              <source src="<?php echo get_sub_field('arquivo')['url']; ?>" type="video/mp4">
+            </video>
           <?php endif; ?>
-        <?php endwhile; ?>
+          <?php if(get_sub_field('arquivo')['type'] === 'image'): ?>
+            <img src="<?php echo get_sub_field('arquivo')['url']; ?>" alt="photo">
+          <?php endif; ?>
+        </div>
+      <?php endwhile; ?>
+    </div>
+    <div class="back-blue">
+      <div class="container">
+        <div class="content"></div>
       </div>
-      <div class="container-person">
-        <div class="container">
-          <div class="back-blue"></div>
+    </div>
+  <?php endif; ?>
+
+  <?php if( have_rows('videos') ): ?>
+    <div class="area-videos">
+      <div class="container">
+        <div class="row">
+          <?php $cont = 0; while( have_rows('videos') ): the_row(); ?>
+            <div class="col-lg-4">
+              <button type="button" class="item-video gs_reveal gs_reveal_fromRight" style="background-image: url(<?php echo get_sub_field('thumb'); ?>)" data-toggle="modal" data-target="#modal-video-<?php echo $cont; ?>">
+                <div class="content">
+                  <img src="<?php bloginfo('template_directory'); ?>/images/btn-play.png" alt="btn-play">
+                  <p><?php echo get_sub_field('titulo'); ?></p>
+                  <p class="d-none link-video"><?php echo get_sub_field('video'); ?></p>
+                </div>
+              </button>
+            </div>
+
+            <div class="modal modal-video fade" id="modal-video-<?php echo $cont; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <video controls>
+                      <source src="<?php echo get_sub_field('video'); ?>" type="video/mp4">
+                    </video>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <?php $cont++; endwhile; ?>
         </div>
       </div>
     </div>
